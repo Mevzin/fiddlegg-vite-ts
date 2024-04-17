c69f9b9ef66546626c7164d3040c4b67cb679b1b
Thiago Torres de Souza <thiagomev@gmail.com>
2024-04-17 02:04:58 -0300

    Add toast message and create new profile card

--- src/Components/WrapperProfile/index.tsx
+++ src/Components/WrapperProfile/index.tsx
@@ -2,6 +2,9 @@ import { useEffect, useState } from "react";
 import { apiBase } from "../../Service/api";
 import { IProfile } from "../../Models/profile";
 import { FaArrowRight } from "react-icons/fa";
+import "./style.css";
+import { Bounce, ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 interface ISummoner {
 	id: string;
@@ -25,28 +28,51 @@ export const WrapperProfile = (props: IProfile) => {
 	async function searchSummoner(name: string, tag: string) {
 		if (!name || !tag) return;
 
-		await apiBase.get(`/league/searchUser/${name}/${tag}`).then((res) => {
-			setSummoner(res.data);
-		});
+		await apiBase
+			.get(`/league/searchUser/${name}/${tag}`)
+			.then((res) => {
+				setSummoner(res.data);
+			})
+			.catch((e) => {
+				console.error(e);
+				setSummoner(undefined);
+				toast.error("Jogador não encontrado", {
+					position: "top-right",
+					icon: false,
+					autoClose: 5000,
+					hideProgressBar: false,
+					closeOnClick: true,
+					pauseOnHover: true,
+					draggable: true,
+					progress: undefined,
+					theme: "colored",
+					transition: Bounce,
+				});
+			});
 
 		return;
 	}
 
 	return (
-		<div className="flex justify-between items-center w-80 h-16 border-2 mt-5 rounded-md text-zinc-50 ">
-			<img
-				className="w-14 rounded-full border-1"
-				src={`${dataLink}/${summoner?.profileIconId}.png`}
-			/>
-			<div className="items-start w-44">
-				<h1>
-					{summoner?.gameName} #{summoner?.tagLine}
-				</h1>
-				<h1>Level: {summoner?.summonerLevel}</h1>
-			</div>
-			<div className="flex h-full w-11 bg-green-500 items-center justify-center">
-				<FaArrowRight />
-			</div>
-		</div>
+		<>
+			{summoner != undefined && (
+				<div className="flex justify-between items-center w-80 h-16 border-2 mt-5 rounded-md text-zinc-50 ">
+					<img
+						className="w-14 rounded-full border-1"
+						src={`${dataLink}/${summoner?.profileIconId}.png`}
+					/>
+					<div className="items-start w-44">
+						<h1>
+							{summoner?.gameName} #{summoner?.tagLine}
+						</h1>
+						<h1>Level: {summoner?.summonerLevel}</h1>
+					</div>
+					<div className="flex h-full w-11 bg-green-500 items-center justify-center hoverShowPage">
+						<FaArrowRight />
+					</div>
+				</div>
+			)}
+			<ToastContainer />
+		</>
 	);
 };
