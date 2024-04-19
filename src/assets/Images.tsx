482b6519320eadf1df84f9fb71eef64dc2e31226
Thiago Torres de Souza <thiagomev@gmail.com>
2024-04-19 03:38:17 -0300

    Changes

--- src/assets/Images.tsx
+++ src/assets/Images.tsx
@@ -1,5 +1,5 @@
 const IMAGES = {
-	image1: new URL("./FIDDLE.GG.SVG", import.meta.url).href,
+	image1: new URL("./FIDDLEGG.svg", import.meta.url).href,
 	image2: new URL("./pngwing.svg", import.meta.url).href,
 };
 
