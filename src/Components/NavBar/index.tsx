139b4ea91649433639516c754f04be184caf919b
Thiago Torres de Souza <thiagomev@gmail.com>
2024-07-24 23:39:53 -0300

    Add link in navbar

--- src/Components/NavBar/index.tsx
+++ src/Components/NavBar/index.tsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import IMAGES from "../../assets/Images";
 
 export const NavBar = () => {
@@ -5,10 +6,12 @@ export const NavBar = () => {
 		<div className=" flex h-16 w-screen items-center justify-center bg-gray-700 border-b-2">
 			<div className="container flex justify-between">
 				<div className="flex ml-12">
-					<div className="flex h-7 mr-8">
-						<img className="mr-3" src={IMAGES.image2} />
-						<img src={IMAGES.image1} />
-					</div>
+					<Link to={'/'}>
+						<div className="flex h-7 mr-8">
+							<img className="mr-3" src={IMAGES.image2} />
+							<img src={IMAGES.image1} />
+						</div>
+					</Link>
 					<input
 						className="h-7 w-80 text-center bg-transparent border-solid border-2 shadow-xl rounded-md text-slate-200 border-zinc-400"
 						type="text"
